2ffd8bce3e97545522f9b720b77f008dcb453fd6
EdvardBruun <ebruun@princeton.edu>
2021-04-19 21:51:17 -0400

    tried to implement gradient descent, not there yet

--- main.py
+++ main.py
@@ -3,32 +3,38 @@ import matplotlib.pyplot as plt
 import random
 
 #LOCAL IMPORTS
-from graphs import Graph, generate_graph
+from graphs import Graph, generate_graph, generate_graph_guess
 from analysis import Analysis
 from graph_plot import plot, plot_iterations, plot_animations
 
-from inputs.input_structure import p1, e1 #pre-defined structures
-from inputs.input_init_conditions import ic1, ic2 #pre-defined structures
+from inputs.input_structure import p1, e1, p2, e2 #pre-defined structures
+from inputs.input_init_conditions import ic1_1, ic1_1, ic2_1 #pre-defined structures
 
 
 ##########################################################
 # 1. Create target structure
 ##########################################################
-random.seed(10)
-gen_size = 20
+random.seed(12)
+gen_size = 6
 
-vertices = p1
-edges = e1
 #vertices, edges = generate_graph(n = gen_size, fac = 1)
 
-G1 = Graph(vertex_list = vertices, edge_list = edges)
+vertices = p2
+edges = e2
+edge_lens = [2.0, 2.0, 7/4, 2.0, 197/100, 27/10, 11/5, 29/10, 43/20]
 
+G1 = Graph(vertex_list = vertices, edge_list = edges, edge_lengths=edge_lens)
+
+# vertices = p1
+# edges = e1
+
+# G1 = Graph(vertex_list = vertices, edge_list = edges)
 
 ##########################################################
 # 2. Set initial conditions
 ##########################################################
-vertices_guess = ic1
-#vertices_guess ,_ = generate_graph(n = gen_size, fac = 5)
+vertices_guess = ic2_1
+#vertices_guess = generate_graph_guess(n = gen_size, vertex_list = vertices, fac = 5)
 
 G2 = Graph(vertex_list = vertices_guess, edge_list = edges, rigid_edge = [0])
 
@@ -38,7 +44,7 @@ fig,ax = plot(G1,G2)
 ##########################################################
 # 3. Perform Newton-Raphson analysis
 ##########################################################
-A = Analysis(G2, max_iter=100, btrack = False)
+A = Analysis(G2, max_iter=200, btrack = True)
 A.iterator(G1,G2)
 
 
@@ -46,8 +52,10 @@ A.iterator(G1,G2)
 # 4. Output
 ##########################################################
 #plot_iterations(G1, A.saved_iterations)
-plot_animations(G1, A.saved_iterations, 'outputs/without_backtrack.gif')
+#plot_animations(G1, A.saved_iterations, 'outputs/without_backtrack.gif')
 
 input("Press [enter] to finish.")
+
+
 
 #CODE TO ANALYZE PRODUCED STRUCTURE
