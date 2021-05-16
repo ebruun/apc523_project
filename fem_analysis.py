5c62a6c111b2e736b92207a03657f62576508c4e
isabelmdo <40008482+isabelmdo@users.noreply.github.com>
2021-05-16 10:07:01 -0300

    graph inputs fix

--- fem_analysis.py
+++ fem_analysis.py
@@ -1,41 +1,58 @@
-# external dependencies
+# Python imports
+import random
+
+# External dependencies.
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
-# local functions from truss_element
+# Local functions from truss_element.
 from truss_element import setup
 from truss_element import get_matrices
 from truss_element import get_stresses
 from truss_element import show_results
 
-#PYTHON IMPORTS
-import random
-
-#LOCAL IMPORTS
+# Graph inports.
 from graphs import Graph, generate_graph
+from inputs.medium_size import data_in
 
-from inputs.fem_input_structure import p3, e3, p_validate, e_validate #pre-defined structures
+# Pre-defined structures.
+from inputs.fem_input_structure import p3, e3, p_validate, e_validate
 
 ##########################################################
 # 1. Create target structure and properties.
 ##########################################################
 
 # See predefined structures in the input_structure import above.
-# Option 1:
-vertices = p_validate
-edges = e_validate
+option_1_validate = True
+option_2 = False
 
-# Option 2:
-# vertices = p3
-# edges = e3
-
-# Option 3, generate a random graph uncommenting the next 3 lines:
-# random.seed(12)
-# gen_size = 32 # Variations: 16, 32, 64, 128, 256, 512
-# vertices, edges = generate_graph(n = gen_size, fac = 10)
+# If options 1 and 2 are false, then run option 3 (a random graph)
+if option_1_validate:
+    vertices = p_validate
+    edges = e_validate
+    lengths = data_in["edge_lengths"]
+    plotting_features = data_in["plotting_features1"]
+elif option_2:
+    vertices = p3
+    edges = e3
+    lengths = data_in["edge_lengths"]
+    plotting_features = data_in["plotting_features1"]
+else:
+    # Option 3, generate a random graph:
+    random.seed(12)
+    gen_size = 32 # Variations: 16, 32, 64, 128, 256, 512
+    lengths = []
+    plotting_features = {'n_color':'#afcdfa','e_color': ['k'],'width': 2}
+    vertices, edges = generate_graph(n = gen_size, fac = 10)
 
 # Units in the graph: meters
-G1 = Graph(vertex_list = vertices, edge_list = edges)
+G1 = Graph(
+    vertex_list = vertices,
+    edge_list = edges,
+    edge_lengths = lengths,
+    rigid_edge=[],
+    features = plotting_features
+    )
 
 # Define force applied to last node of graph.
 force_x = 0 # kN
